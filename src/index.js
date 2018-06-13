efe64379e2a68ee5398963325fba96fcfd0c59fe
dandgerson <dandgerson@gmail.com>
2018-06-13 18:07:29 +0300

    guided Lazy loading

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ import './styles/scss/typography.scss';
 
 // libs
 import _ from 'lodash';
+
 let header = document.createElement('h1');
 header.append('Edit your Users... please:');
 document.body.prepend(header);
@@ -20,6 +21,26 @@ if (process.env.NODE_ENV === 'development')
   console.warn('Looks like we are in development mode!'); 
 if (process.env.NODE_ENV === 'production')
   console.warn('We are in production mode!');
+
+function component() {
+  let element = document.createElement('div');
+  let button = document.createElement('button');
+  let br = document.createElement('br');
+
+  button.innerHTML = 'Click me and look at the console!';
+  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  element.append(br, button);
+
+  button.onclick = e => import(/* webpackChunkName: "print" */ './print')
+    .then(module => {
+      let print = module.default;
+
+      print();
+    });
+  return element;
+}
+
+document.body.append(component());
 
 // ####################################################
 
---
+++ src/print.js
@@ -0,0 +1,7 @@
+'use strict';
+
+console.log('The print.js module is loaded! See the network tab in dev tools...');
+
+export default () => {
+  console.log('Button Clicked: Here\'s "some text"!');
+};
